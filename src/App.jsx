0ffc2403280ca8898a349d05662262617ec9d8c2
Anubhav-singhx <157218245+Anubhav-singhx@users.noreply.github.com>
2025-01-21 19:40:00 +0530

    Code Updation

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import About from "./sections/About"
+import Hero from "./sections/Hero"
+import Navbar from "./sections/Navbar"
+import Projects from "./sections/Projects"
+
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto text-white'>
+    <Navbar/>
+    <Hero/>
+    <About/>
+    <Projects/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/DemoComputer.jsx
@@ -0,0 +1,10 @@
+
+const DemoComputer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DemoComputer
